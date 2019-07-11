c2482bd190999fe67b1e15f9bb3b6e166211675e
yangyang0871 <yangyang0871@gamil.com>
2019-07-11 18:04:45 +0930

    user current location/ load place details

--- Android/PlaceBook/app/src/main/java/com/example/yyang/placebook/MapsActivity.kt
+++ Android/PlaceBook/app/src/main/java/com/example/yyang/placebook/MapsActivity.kt
@@ -1,18 +1,37 @@
 package com.example.yyang.placebook
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.Places
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
+    //    private var locationRequest: LocationRequest? = null
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var googleApiClient: GoogleApiClient
+
+
+    companion object{
+        private const val REQUEST_LOCATION = 1
+        private const val TAG = "MapsActivity"
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +40,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setupLocationClient()
     }
 
     /**
@@ -34,10 +54,96 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        getCurrentLocation()
+        mMap.setOnPoiClickListener {
+            Toast.makeText(this, it.name, Toast.LENGTH_LONG).show()
+        }
     }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.e(TAG, "Google play connection failed: " +
+                p0.errorMessage)
+    }
+
+
+    private  fun setupLocationClient(){
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private  fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION)
+    }
+
+    private fun getCurrentLocation(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED){
+                requestLocationPermissions()
+        }else{
+            mMap.isMyLocationEnabled = true
+
+//            if (locationRequest == null){
+//                locationRequest = LocationRequest.create()
+//                locationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//                locationRequest?.interval = 5000
+//                locationRequest?.fastestInterval = 1000
+//
+//                val locationCallBack = object : LocationCallback() {
+//                    override fun onLocationResult(locationResult: LocationResult?) {
+//                        getCurrentLocation()
+//                    }
+//                }
+//
+//                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallBack, null)
+//            }
+
+            fusedLocationClient.lastLocation.addOnCompleteListener{
+                if (it.result != null && it.result?.latitude != null && it.result?.longitude != null){
+                    val latlng = LatLng(it.result!!.latitude, it.result!!.longitude)
+//                    mMap.clear()
+//                    mMap.addMarker(MarkerOptions().position(latlng).title("You are here!"))
+                    val update = CameraUpdateFactory.newLatLngZoom(latlng, 16.0f)
+                    mMap.moveCamera(update)
+                }else{
+                    Log.e(TAG,"No location found")
+                }
+            }
+
+        }
+
+
+    }
+
+    private fun  setGoogleClient(){
+        googleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Places.GEO_DATA_API)
+            .build()
+    }
+
+    private fun displayPoi(pointOfInterest: PointOfInterest) {
+        // 1
+        Places.GeoDataApi.getPlaceById(googleApiClient,
+            pointOfInterest.placeId)
+            // 2
+            .setResultCallback { places ->
+                // 3
+                if (places.status.isSuccess && places.count > 0) {
+                    // 4
+                    val place = places.get(0)
+                    // 5
+                    Toast.makeText(this,
+                        "${place.name} ${place.phoneNumber}",
+                        Toast.LENGTH_LONG).show()
+                } else {
+                    Log.e(TAG,
+                        "Error with getPlaceById ${places.status.statusMessage}")
+                }
+// 6
+                places.release()
+            }
+    }
+
+
 }
